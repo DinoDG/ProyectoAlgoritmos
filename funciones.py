18be2d4b2f834a626b8127140a4c3d3a2b83db0f
Danilo <106288930+DinoDG@users.noreply.github.com>
2022-06-08 20:28:19 -0400

    Funcion Selec_Asientos

--- funciones.py
+++ funciones.py
@@ -1,3 +1,20 @@
-def hola():
-    print("Hola")
-    return
+def Selec_Asiento(nroPedido):
+  
+  nroAsiento=0
+  proceso=True
+  while proceso:
+    for x in range(len(columna)):   
+      for j in range(len(aux)):      
+        nroAsiento +=1
+        if nroPedido==nroAsiento:
+          if columna[x][j]=="X":
+            print("Asiento no disponible")
+            proceso=False
+            return False
+            break
+          else:
+            columna[x][j]="X"
+            proceso=False
+            return True
+            break
+            
