3596726bb28e8291c21a15c7a8af038decb9b5b4
jpguital <106289025+jpguital@users.noreply.github.com>
2022-06-09 00:09:04 -0400

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,5 +1,21 @@
 
+valorVIP=240000
+valorNORM=78900
+contNORM,contVIP=0,0
 
+valido=True
+#Creacion avion
+columna=[]
+fila=[]
+aux=[]
+contAs=0
+for x in range(14):
+    for j in range(3):
+        contAs +=1
+        fila.append(contAs)
+        aux=fila.copy()
+    columna.append(aux)
+    fila.clear()
 
 def Selec_Asiento(nroPedido):
   
