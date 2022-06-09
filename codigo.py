6c775974998877d04c07d31b2c4dced385e716d2
jpguital <106289025+jpguital@users.noreply.github.com>
2022-06-09 00:11:35 -0400

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,3 +1,20 @@
 import funciones as fn
 
 fn.hola()
+valorVIP=240000
+valorNORM=78900
+contNORM,contVIP=0,0
+
+valido=True
+#Creacion avion
+columna=[]
+fila=[]
+aux=[]
+contAs=0
+for x in range(14):
+    for j in range(3):
+        contAs +=1
+        fila.append(contAs)
+        aux=fila.copy()
+    columna.append(aux)
+    fila.clear()
